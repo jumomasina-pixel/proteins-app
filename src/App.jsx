54071d59d5163f47c527476a19ffa3d2c05b912a
John Masina <yumo@Johns-MacBook-Pro.local>
2026-05-19 21:12:28 +1000

    version bump - fresh onboarding for all users

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useState, useRef, useEffect, useMemo } from 'react'
 // Any existing storage that doesn't carry a matching version will be wiped and
 // the user will restart from the welcome screen as a new user.
 
-const PROFILE_VERSION = 3
+const PROFILE_VERSION = 4
 
 const LS_KEYS = ['lhc_profile', 'lhc_saved_recipes', 'lhc_sessions', 'lhc_streak', 'lhc_stats']
 
